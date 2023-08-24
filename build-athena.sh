1575b4974187d06cbf6670e082039bfa72e663aa
Jesús Higueras <jesus@dabbleam.com>
2023-08-24 21:56:54 +0200

    Let there be gzip!

--- build-athena.sh
+++ build-athena.sh
@@ -9,4 +9,4 @@ make CONFIG_BBRY=1 O=out CROSS_COMPILE=$CROSS_COMPILE ARCH=$ARCH mrproper
 make CONFIG_BBRY=1 O=out CROSS_COMPILE=$CROSS_COMPILE ARCH=$ARCH athena-perf_defconfig
 # use config extracted from device for now
 cp config-from-athena out/.config
-make CONFIG_BBRY=1 O=out CROSS_COMPILE=$CROSS_COMPILE ARCH=$ARCH -j24
+make CONFIG_BBRY=1 O=out CROSS_COMPILE=$CROSS_COMPILE ARCH=$ARCH -j24 Image.gz
