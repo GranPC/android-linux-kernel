072846e17215a8c717243cc33f9b0adde7e4b040
Jesús Higueras <jesus@dabbleam.com>
2023-08-24 21:29:41 +0200

    Let there be athena!

--- build-athena.sh
+++ build-athena.sh
@@ -6,7 +6,7 @@ export CROSS_COMPILE=/home/granpc/android/prebuilts/gcc/linux-x86/aarch64/aarch6
 make CONFIG_BBRY=1 O=out CROSS_COMPILE=$CROSS_COMPILE ARCH=$ARCH mrproper
 
 # TODO: perf?
-make CONFIG_BBRY=1 O=out CROSS_COMPILE=$CROSS_COMPILE ARCH=$ARCH luna-perf_defconfig
+make CONFIG_BBRY=1 O=out CROSS_COMPILE=$CROSS_COMPILE ARCH=$ARCH athena-perf_defconfig
 # use config extracted from device for now
-cp config-from-luna out/.config
-make CONFIG_BBRY=1 O=out CROSS_COMPILE=$CROSS_COMPILE ARCH=$ARCH -j12
+cp config-from-athena out/.config
+make CONFIG_BBRY=1 O=out CROSS_COMPILE=$CROSS_COMPILE ARCH=$ARCH -j24
